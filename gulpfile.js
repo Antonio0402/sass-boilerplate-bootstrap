7948a774003ed36d514415b33046fa0f7604bf86
Antonio0402 <21210003@ms.uit.edu.vn>
2025-04-01 16:52:14 +0700

    Add browserslist configuration and enhance Gulp tasks with sourcemaps and postcss-preset-env

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 import gulp from 'gulp';
-const { src, dest, watch, series } = 'gulp';
+const { src, dest, watch, series } = gulp;
 import gulpSass from "gulp-sass";
 import * as sassEmbedded from "sass";
 const sass = gulpSass(sassEmbedded);
 import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
+import postcssPresetEnv from 'postcss-preset-env';
 import postcss from 'gulp-postcss';
 const browserSync = require('browser-sync').create();
 import purgecss from 'gulp-purgecss';
@@ -14,24 +15,35 @@ import concat from "gulp-concat";
 
 /* Sass Task */
 function scssTask() {
-  return src('./scss/style.scss')
-    .pipe(sass().on('error', sass.logError))
+  return src('./scss/style.scss', { sourcemaps: true }) // Source file
+    .pipe(sass().on('error', sass.logError)) // Compile Sass
     .pipe(purgecss({
-      content: ['*.html']
+      content: ['*.html'] // Remove unused CSS
     }))
-    .pipe(postcss([autoprefixer, cssnano()]))
-    .pipe(dest('./dist/css', { sourcemaps: '.' }))
-    .pipe(browserSync.stream());
+    .pipe(postcss([
+      postcssPresetEnv({
+        stage: 3,
+        features: {
+          "logical-properties-and-values": false,
+          "opacity-percentage": true,
+          "text-decoration-shorthand": true,
+        }
+      }), // Use postcss-preset-env with stage 1 features
+      autoprefixer(), // Add vendor prefixes
+      cssnano() // Minify CSS
+    ]))
+    .pipe(dest('./dist/css', { sourcemaps: '.' })) // Write sourcemaps
+    .pipe(browserSync.stream()); // Stream changes to BrowserSync
 }
 
 /* Js Task */
 function jsTask() {
-  return src('app/js/*.js')
+  return src('./js/*.js', { sourcemaps: true }) // Source file
     .pipe(init())
     .pipe(terser())
     .pipe(concat('main.js'))
     .pipe(write('.'))
-    .pipe(dest('dist/js'));
+    .pipe(dest('dist/js', { sourcemaps: '.' })) // Write sourcemaps;
 }
 
 
@@ -53,11 +65,12 @@ function browserSyncReload(cb) {
 
 /* Watch Task */
 function watchTask() {
-  watch(['./scss/**/*.scss', '*.html'], series(scssTask, browserSyncReload));
+  watch(['./scss/**/*.scss', './js/**/*.js', '*.html'], series(scssTask, jsTask, browserSyncReload));
 }
 
 const _default = series(
   scssTask,
+  jsTask,
   browserSyncServe,
   watchTask
 );
